c6e2980bee3e1476bd9e646ceaf62d3be433bdb7
IlyaG <ilyag@uveye.com>
2024-07-14 13:35:27 +0000

    Installable depth_anything_v2 library

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+import uv_build_utils.version_utils
+import uv_build_utils.repo_utils
+
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
+
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=PipSession())
+
+setuptools.setup(
+    name="depth_anything_v2",
+    version=uv_build_utils.version_utils.version_string(),
+    author="",
+    author_email="",
+    description="",
+    packages=setuptools.find_packages(exclude=['*metric_depth*', '*assets*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[str(requirement.requirement) for requirement in requirements],
+    include_package_data=True
+)
